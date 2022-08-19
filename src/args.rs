beda7377cc9cbccfa00c0ed9c163504580e3cbf3
Mikolaj <kolek.mikolaj@gmail.com>
2022-08-19 11:28:27 +0200

    Make the program work even with extremely large outputs

---
+++ src/args.rs
@@ -0,0 +1,26 @@
+use clap::Parser;
+
+/// A simple tester for competitive programming exercises
+#[derive(Parser, Debug)]
+#[clap(name = "Tester", version, about, long_about = None)]
+pub struct Args {
+    /// Input directory
+    #[clap(short, long, value_parser, default_value = "in")]
+    pub r#in: String,
+
+    /// Output directory
+    #[clap(short, long, value_parser, default_value = "out")]
+    pub out: String,
+
+    /// The number of seconds after which a test or generation times out if the program does not return
+    #[clap(short, long, value_parser, default_value = "5")]
+    pub timeout: u64,
+
+    /// Makes the tester generate output files in the output directory instead of comparing the program's output with the files in the output directory
+    #[clap(short, long, action)]
+    pub generate: bool,
+
+    /// The name of the file containing the source code
+    #[clap(value_parser)]
+    pub filename: String
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,27 @@
+mod args;
+
 use std::{fs};
 use std::cmp::max;
 use std::env::current_dir;
 use std::fmt::{Write as FmtWrite};
 use std::fs::{File, read_dir};
-use std::io::Read;
 use std::path::Path;
-use std::process::{Command, Stdio};
+use std::process::{Command};
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 use ansi_term::Color::{Green, Red};
 use atomic_counter::{AtomicCounter, RelaxedCounter};
-use clap::Parser;
 use indicatif::{ParallelProgressIterator, ProgressState, ProgressStyle};
 use lazy_static::lazy_static;
 use rayon::iter::{IntoParallelRefIterator};
 use rayon::prelude::*;
+use tempfile::tempdir;
 use term_table::{Table, TableStyle};
 use term_table::row::Row;
 use term_table::table_cell::{Alignment, TableCell};
 use wait_timeout::ChildExt;
-
-/// A simple tester for competitive programming exercises
-#[derive(Parser, Debug)]
-#[clap(name = "Tester", version, about, long_about = None)]
-struct Args {
-    /// Input directory
-    #[clap(short, long, value_parser, default_value = "in")]
-    r#in: String,
-
-    /// Output directory
-    #[clap(short, long, value_parser, default_value = "out")]
-    out: String,
-
-    /// The number of seconds after which a test or generation times out if the program does not return
-    #[clap(short, long, value_parser, default_value = "5")]
-    timeout: u64,
-
-    /// Makes the tester generate output files in the output directory instead of comparing the program's output with the files in the output directory
-    #[clap(short, long, action)]
-    generate: bool,
-
-    /// The name of the file containing the source code
-    #[clap(value_parser, default_value = "dom.cpp")]
-    filename: String
-}
+use args::Args;
+use clap::Parser;
 
 lazy_static! {
     static ref CORRECT: RelaxedCounter = RelaxedCounter::new(0);
@@ -57,6 +35,7 @@ fn main() {
     let output_dir: String = format!("{}/{}", &workspace_dir, args.out);
     let executable = format!("{}.o", Path::new(&args.filename).file_stem().unwrap().to_str().unwrap());
 
+    // Compilation
     Command::new("g++")
         .args(["-std=c++17", "-O3", "-static", &args.filename, "-o", &executable])
         .output().unwrap();
@@ -65,37 +44,31 @@ fn main() {
         .unwrap()
         .with_key("eta", |state: &ProgressState, w: &mut dyn FmtWrite| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
         .progress_chars("#>-");
-
     if !args.generate {
         style = style.with_key("correct", |_state: &ProgressState, w: &mut dyn FmtWrite| write!(w, "{}", Green.paint(format!("{} correct", &CORRECT.get()))).unwrap())
             .with_key("incorrect", |_state: &ProgressState, w: &mut dyn FmtWrite| write!(w, "{}", Red.paint(format!("{} incorrect", &INCORRECT.get()))).unwrap());
     }
 
     let slowest_test: Arc<Mutex<(f64, String)>> = Arc::new(Mutex::new((-1 as f64, "PLACEHOLDER".parse().unwrap())));
-    let errors = Arc::new(Mutex::new(vec![]));
+    let errors: Arc<Mutex<Vec<(String, String, String)>>> = Arc::new(Mutex::new(vec![]));
     let before_testing = Instant::now();
     read_dir(&input_dir).unwrap().collect::<Vec<_>>().par_iter().progress_with_style(style).for_each(|file| {
+        let test_name = file.as_ref().unwrap().path().file_stem().unwrap().to_str().unwrap().to_string();
+
         let input_file = File::open(file.as_ref().unwrap().path()).unwrap();
+        let tempdir = tempdir().unwrap();
+        let tempfile_path = tempdir.path().join(format!("{}.out", test_name));
+        let tempfile = File::create(&tempfile_path).unwrap();
 
         let start = Instant::now();
-        let mut child = Command::new(format!("./{}", &executable))
-            .stdout(Stdio::piped())
+        Command::new(format!("./{}", &executable))
+            .stdout(tempfile)
             .stdin(input_file)
-            .spawn().unwrap();
-        let output_str = match child.wait_timeout(Duration::from_secs(args.timeout)).unwrap() {
-            Some(_) => {
-                let mut res = String::new();
-                child.stdout.unwrap().read_to_string(&mut res).unwrap();
-                res
-            }
-            None => {
-                child.kill().unwrap();
-                "The program timed out".to_string()
-            }
-        };
+            .spawn().unwrap().wait_timeout(Duration::from_secs(args.timeout)).unwrap();
+
+        let output_str: String = fs::read_to_string(tempfile_path).unwrap();
         let test_time = start.elapsed().as_secs_f64();
 
-        let test_name = file.as_ref().unwrap().path().file_stem().unwrap().to_str().unwrap().to_string();
         let clone = Arc::clone(&slowest_test);
         let mut slowest_test_mutex = clone.lock().unwrap();
         if test_time > slowest_test_mutex.0 {
