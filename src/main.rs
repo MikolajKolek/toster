c77641af7b1ec82d67c3b4e6e93901465207407b
Mikolaj <kolek.mikolaj@gmail.com>
2022-08-19 13:28:05 +0200

    Improve the diff table shown for incorrect answers (make it always fit the screen and fix color bleed onto table)

--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,12 @@ use lazy_static::lazy_static;
 use rayon::iter::{IntoParallelRefIterator};
 use rayon::prelude::*;
 use tempfile::tempdir;
-use term_table::{Table, TableStyle};
-use term_table::row::Row;
-use term_table::table_cell::{Alignment, TableCell};
 use wait_timeout::ChildExt;
 use args::Args;
 use clap::Parser;
+use comfy_table::{Attribute, Cell, Color, Table};
+use comfy_table::ContentArrangement::Dynamic;
+use terminal_size;
 
 lazy_static! {
     static ref CORRECT: RelaxedCounter = RelaxedCounter::new(0);
@@ -117,37 +117,36 @@ fn main() {
                 let split_file = file_out.split("\n").collect::<Vec<_>>();
                 let split_out = program_out.split("\n").collect::<Vec<_>>();
                 if max(split_file.len(), split_out.len()) <= 100 {
-                    let mut table = Table::new();
-                    table.max_column_width = 40;
-                    table.style = TableStyle::extended();
+                    let (terminal_size::Width(w), terminal_size::Height(_)) = terminal_size::terminal_size().unwrap();
 
-                    table.add_row(Row::new(vec![
-                        TableCell::new(Green.paint("Output file")),
-                        TableCell::new_with_alignment(Red.paint("Your program's output"), 1, Alignment::Right)
-                    ]));
+                    let mut table = Table::new();
+                    table
+                        .set_content_arrangement(Dynamic)
+                        .set_width(w)
+                        .set_header(vec![
+                            Cell::new("Output file").add_attribute(Attribute::Bold).fg(Color::Green),
+                            Cell::new("Your program's output").add_attribute(Attribute::Bold).fg(Color::Red)
+                        ]);
 
                     for i in 0..max(split_file.len(), split_out.len()) {
                         let file_segment = if split_file.len() > i { split_file[i] } else { "" };
                         let out_segment = if split_out.len() > i { split_out[i] } else { "" };
 
                         if file_segment != out_segment {
-                            table.add_row(Row::new(vec![
-                                TableCell::new(Green.paint(file_segment)),
-                                TableCell::new_with_alignment(Red.paint(out_segment), 1, Alignment::Right)
-                            ]));
+                            table.add_row(vec![
+                                Cell::new(file_segment).fg(Color::Green),
+                                Cell::new(out_segment).fg(Color::Red)
+                            ]);
                         }
                         else {
-                            table.add_row(Row::new(vec![
-                                TableCell::new(file_segment),
-                                TableCell::new_with_alignment(out_segment, 1, Alignment::Right)
-                            ]));
+                            table.add_row(vec![file_segment, out_segment]);
                         }
                     }
 
-                    println!("{}", table.render());
+                    println!("{table}");
                 }
                 else {
-                    println!("{}", Red.paint(""))
+                    println!("{}", Red.paint("Output too large to show"))
                 }
             }
         }
