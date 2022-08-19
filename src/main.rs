466be2910e8704e7010bfbf1f9558a718011975c
Mikolaj <kolek.mikolaj@gmail.com>
2022-08-19 21:29:30 +0200

    Cleanups and refactors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod args;
-mod test_error;
+mod test_result;
 
 use std::{fs};
 use std::env::current_dir;
 use std::fmt::{Write as FmtWrite};
 use std::fs::{DirEntry, File, read_dir};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::{Command};
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
@@ -14,17 +14,42 @@ use indicatif::{ParallelProgressIterator, ProgressState, ProgressStyle};
 use lazy_static::lazy_static;
 use rayon::iter::{IntoParallelRefIterator};
 use rayon::prelude::*;
-use tempfile::tempdir;
+use tempfile::{tempdir};
 use wait_timeout::ChildExt;
 use args::Args;
 use clap::Parser;
 use colored::Colorize;
-use crate::test_error::TestError;
-use crate::TestError::{NoOutputFile, TimedOut, Incorrect};
+use crate::test_result::TestResult;
+use crate::TestResult::{NoOutputFile, TimedOut, Incorrect};
 
 lazy_static! {
     static ref CORRECT: RelaxedCounter = RelaxedCounter::new(0);
     static ref INCORRECT: RelaxedCounter = RelaxedCounter::new(0);
+}
+
+fn compile_cpp(source_code_file: PathBuf) -> Result<String, String> {
+	let source_code_folder = source_code_file.parent().expect("The source code is in an invalid folder!");
+	let executable_file_base = source_code_folder.join(source_code_file.file_stem().expect("The provided filename is invalid!"));
+	let executable_file = format!("{}.o", executable_file_base.to_str().expect("The provided filename is invalid!"));
+	let tempdir = tempdir().expect("Failed to create temporary directory!");
+	let compilation_result_path = tempdir.path().join(format!("{}.out", executable_file_base.to_str().expect("The provided filename is invalid!")));
+	let compilation_result = File::create(&compilation_result_path).expect("Failed to create temporary file!");
+
+	let time_before_compilation = Instant::now();
+	Command::new("g++")
+		.args(["-std=c++17", "-O3", "-static", source_code_file.to_str().expect("The provided filename is invalid!"), "-o", &executable_file])
+		.stderr(compilation_result)
+		.spawn().expect("g++ failed to start").wait_timeout(Duration::from_secs(10)).expect("Compilation took too long!");
+	let compilation_time = time_before_compilation.elapsed().as_secs_f64();
+
+	let compilation_result = fs::read_to_string(&compilation_result_path).expect("Failed to read G++ output");
+	return if !compilation_result.is_empty() {
+		Err(compilation_result)
+	}
+	else {
+		println!("{}", format!("Compilation completed in {:.2}s", compilation_time).green());
+		Ok(executable_file)
+	}
 }
 
 fn main() {
@@ -32,32 +57,30 @@ fn main() {
 	let workspace_dir = current_dir().expect("The current directory is invalid!").to_str().expect("The current directory is invalid!").to_string();
 	let input_dir: String = format!("{}/{}", &workspace_dir, args.r#in);
 	let output_dir: String = format!("{}/{}", &workspace_dir, args.out);
-	let executable = format!("{}.o", Path::new(&args.filename).file_stem().expect("The provided filename is invalid!").to_str().expect("The provided filename is invalid!"));
-	let tempdir = tempdir().expect("Failed to create temporary directory");
+	let tempdir = tempdir().expect("Failed to create temporary directory!");
 
+	// Making sure that the input and output directories as well as the source code file exist
+	if !Path::new(&output_dir).is_dir() {
+		if args.generate {
+			fs::create_dir(&output_dir).expect("Failed to create output directory!");
+		}
+		else {
+			println!("{}", "The output directory does not exist".red());
+			return;
+		}
+	}
 	if !Path::new(&input_dir).is_dir() { println!("{}", "The input directory does not exist".red()); return; }
-	if args.generate && !Path::new(&output_dir).is_dir() {
-		fs::create_dir(&output_dir).expect("Failed to create output directory!");
-	}
-	else {
-		if !Path::new(&output_dir).is_dir() { println!("{}", "The output directory does not exist".red());return; }
-	}
 	if !Path::new(&args.filename).is_file() { println!("{}", "The source code file does not exist".red()); return; }
 
-	let compilation_tempfile_path = tempdir.path().join(format!("{}.out", executable));
-	let compilation_tempfile = File::create(&compilation_tempfile_path).expect("Failed to create temporary file!");
-	let time_before_compilation = Instant::now();
-	Command::new("g++")
-		.args(["-std=c++17", "-O3", "-static", &args.filename, "-o", &executable])
-		.stderr(compilation_tempfile)
-		.spawn().expect("g++ failed to start").wait_timeout(Duration::from_secs(10)).expect("Compilation took too long!");
-	let gpp_out = fs::read_to_string(compilation_tempfile_path).expect("Failed to read temporary file!");
-	if !gpp_out.is_empty() {
-		println!("{}\n{}", "Compilation failed with the following errors:".red(), gpp_out);
-		return;
-	}
-	else {
-		println!("{}", format!("Compilation completed in {:.2}s", time_before_compilation.elapsed().as_secs_f64()).green())
+	// Compiling
+	let executable: String;
+	match compile_cpp(Path::new(&args.filename).to_path_buf()) {
+		Ok(result) => { executable = result }
+		Err(error) => {
+			println!("{}", "Compilation failed with the following errors:".red());
+			println!("{}", error);
+			return;
+		}
 	}
 
 	let mut style: ProgressStyle = ProgressStyle::with_template("[{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} ({eta})\n{correct} {incorrect}")
@@ -70,7 +93,7 @@ fn main() {
 	}
 
 	let slowest_test: Arc<Mutex<(f64, String)>> = Arc::new(Mutex::new((-1 as f64, String::new())));
-	let errors: Arc<Mutex<Vec<TestError>>> = Arc::new(Mutex::new(vec![]));
+	let errors: Arc<Mutex<Vec<TestResult>>> = Arc::new(Mutex::new(vec![]));
 	let before_testing = Instant::now();
 	read_dir(&input_dir).expect("Cannot open input directory!").collect::<Vec<_>>().par_iter().progress_with_style(style).for_each(|input| {
 		let file: &DirEntry = input.as_ref().expect("Failed to acquire reference!");
@@ -102,9 +125,9 @@ fn main() {
 		let test_time = start.elapsed().as_secs_f64();
 		let output: String = fs::read_to_string(&test_output_file_path).expect("Failed to read temporary file!");
 
-		let clone = Arc::clone(&slowest_test);
-		let mut slowest_test_mutex = clone.lock().expect("Failed to acquire mutex!");
-		if test_time > slowest_test_mutex.0 {
+
+		let slowest_test_clone = Arc::clone(&slowest_test);
+		let mut slowest_test_mutex = slowest_test_clone.lock().expect("Failed to acquire mutex!");		if test_time > slowest_test_mutex.0 {
 			*slowest_test_mutex = (test_time, test_name.clone());
 		}
 
@@ -115,7 +138,7 @@ fn main() {
 
 			INCORRECT.inc();
 			let clone = Arc::clone(&errors);
-			clone.lock().expect("Failed to acquire mutex!").push(TimedOut { test_name });
+			clone.lock().expect("Failed to acquire mutex!").push(TimedOut {test_name});
 			return;
 		}
 
@@ -123,7 +146,7 @@ fn main() {
 			if !Path::new(&output_file).is_file() {
 				INCORRECT.inc();
 				let clone = Arc::clone(&errors);
-				clone.lock().expect("Failed to acquire mutex!").push(/*"".to_string(), "".to_string(), "".to_string(), */NoOutputFile {test_name});
+				clone.lock().expect("Failed to acquire mutex!").push(NoOutputFile {test_name});
 				return;
 			}
 
@@ -131,7 +154,7 @@ fn main() {
 			if output.split_whitespace().collect::<Vec<&str>>() != output_file_contents.split_whitespace().collect::<Vec<&str>>() {
 				INCORRECT.inc();
 				let clone = Arc::clone(&errors);
-				clone.lock().expect("Failed to acquire mutex!").push(/*test_name, output, output_file_contents,*/ /*"".to_string(), "".to_string(), "".to_string(), */Incorrect {test_name, correct_answer: output_file_contents, incorrect_answer: output });
+				clone.lock().expect("Failed to acquire mutex!").push(Incorrect {test_name, correct_answer: output_file_contents, incorrect_answer: output});
 			}
 			else {
 				CORRECT.inc();
@@ -139,40 +162,39 @@ fn main() {
 		}
 	});
 
+
+
+	// Printing the output
+	let testing_time = before_testing.elapsed().as_secs_f64();
 	let slowest_test_clone = Arc::clone(&slowest_test);
-	let slowest_test_mutex = slowest_test_clone.lock().expect("Failed to acquire mutex!");
 	let errors_clone = Arc::clone(&errors);
+	let slowest_test_mutex = slowest_test_clone.lock().expect("Failed to acquire mutex!");
 	let errors_mutex = errors_clone.lock().expect("Failed to acquire mutex!");
-	if !args.generate {
-		println!("Testing finished in {:.2}s with {} and {}: (Slowest test: {} at {:.3}s)",
-		         before_testing.elapsed().as_secs_f64(),
-		         format!("{} correct answers", CORRECT.get()).green(),
-		         format!("{} incorrect answers", INCORRECT.get()).red(),
-		         slowest_test_mutex.1,
-		         slowest_test_mutex.0
-		);
-
-		if !errors_mutex.is_empty() {
-			println!("Errors were found in the following tests:");
-
-			for test_error in errors_mutex.iter() {
-				println!("{}", test_error.to_string());
-			}
+	match args.generate {
+		true => {
+			println!("Generation finished in {:.2}s (Slowest test: {} at {:.3}s)",
+			         testing_time,
+			         slowest_test_mutex.1,
+			         slowest_test_mutex.0
+			);
+		}
+		false => {
+			println!("Testing finished in {:.2}s with {} and {}: (Slowest test: {} at {:.3}s)",
+			         testing_time,
+			         format!("{} correct answers", CORRECT.get()).green(),
+			         format!("{} incorrect answers", INCORRECT.get()).red(),
+			         slowest_test_mutex.1,
+			         slowest_test_mutex.0
+			);
 		}
 	}
-	else {
-		println!("Program finished in {:.2}s (Slowest test: {} at {:.3}s)",
-		         before_testing.elapsed().as_secs_f64(),
-		         slowest_test_mutex.1,
-		         slowest_test_mutex.0
-		);
 
-		if !errors_mutex.is_empty() {
-			println!("Errors were found in the following tests:");
+	// Printing errors if necessary
+	if !errors_mutex.is_empty() {
+		println!("Errors were found in the following tests:");
 
-			for test_error in errors_mutex.iter() {
-				println!("{}", test_error.to_string());
-			}
+		for test_error in errors_mutex.iter() {
+			println!("{}", test_error.to_string());
 		}
 	}
 }
