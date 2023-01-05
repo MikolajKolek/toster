0a0eb16c50fb705f407d17cbfd46bfbb4940631e
Mikolaj <kolek.mikolaj@gmail.com>
2023-01-05 19:16:15 +0100

    Add description for Ctrl+C feature to help page

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,7 @@
 use clap::Parser;
 
-/// A simple-as-toast tester for C++ solutions to competitive programming exercises
 #[derive(Parser, Debug)]
-#[clap(name = "Toster", version, about, long_about = None)]
+#[command(name = "Toster", version, about = "A simple-as-toast tester for C++ solutions to competitive programming exercises\nPress Ctrl+C during testing to stop before it finishes and see current results", long_about = None)]
 pub struct Args {
     /// Input directory
     #[clap(short, long, value_parser, default_value = "in")]
