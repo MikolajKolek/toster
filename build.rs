a7495ed17db22067247220b5d9dc21f22ced55e8
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2024-04-18 09:00:53 +0200

    Use path methods in build.rs

--- build.rs
+++ build.rs
@@ -16,21 +16,18 @@ fn main() {
 		println!("cargo:warning=Couldn't locate the user's executable directory. Sio2jail was not installed");
 		process::exit(0);
 	});
-	let executable_dir_str = executable_dir.to_str().unwrap_or_else(|| {
-		println!("cargo:warning=The user's executable directory is invalid. Sio2jail was not installed");
-		process::exit(0);
-	});
+	let sio2jail_path = executable_dir.join("sio2jail");
 
-	fs::create_dir_all(executable_dir_str).unwrap_or_else(|_| {
-		println!("cargo:warning=Couldn't create the {} directory. Sio2jail was not installed", executable_dir_str);
+	fs::create_dir_all(executable_dir).unwrap_or_else(|_| {
+		println!("cargo:warning=Couldn't create the {} directory. Sio2jail was not installed", executable_dir.display());
 		process::exit(0);
 	});
-	fs::copy("sio2jail", format!("{}/sio2jail", executable_dir_str)).unwrap_or_else(|_| {
-		println!("cargo:warning=Couldn't copy sio2jail to {}. Sio2jail was not installed", executable_dir_str);
+	fs::copy("sio2jail", &sio2jail_path).unwrap_or_else(|_| {
+		println!("cargo:warning=Couldn't copy sio2jail to {}. Sio2jail was not installed", executable_dir.display());
 		process::exit(0);
 	});
-	fs::set_permissions(format!("{}/sio2jail", executable_dir_str), Permissions::from_mode(0o755)).unwrap_or_else(|_| {
-		println!("cargo:warning=Couldn't set execute permissions on sio2jail at {}. Sio2jail was not installed", format!("{}/sio2jail", executable_dir_str));
+	fs::set_permissions(&sio2jail_path, Permissions::from_mode(0o755)).unwrap_or_else(|_| {
+		println!("cargo:warning=Couldn't set execute permissions on sio2jail at {}. Sio2jail was not installed", sio2jail_path.display());
 		process::exit(0);
 	});
 }
