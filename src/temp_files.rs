3e59f3c27772601e2924413020797f889ec3f85b
Dominik <29484605+dominik-korsa@users.noreply.github.com>
2024-05-17 14:13:25 +0200

    Fix create_temp_file() on Windows (#47)

--- src/temp_files.rs
+++ src/temp_files.rs
@@ -1,8 +1,6 @@
 use std::fs::File;
 use std::io;
 use std::process::Stdio;
-#[cfg(target_os = "linux")]
-use memfile::MemFile;
 
 pub(crate) fn make_cloned_stdio(file: &File) -> Stdio {
     Stdio::from(file.try_clone().unwrap())
@@ -15,13 +13,17 @@ pub(crate) fn make_cloned_stdio(file: &File) -> Stdio {
 ///
 /// Always returns a `File` struct
 pub(crate) fn create_temp_file() -> io::Result<File> {
-    if cfg!(target_os = "linux") {
+    #[cfg(target_os = "linux")]
+    {
         // The file is deleted when all file descriptors are closed
         // https://man7.org/linux/man-pages/man2/memfd_create.2.html
-        MemFile::create_default("toster temporary file")
+        memfile::MemFile::create_default("toster temporary file")
             .map(|memfile| memfile.into_file())
-    } else {
-        // tempfile() adds FILE_FLAG_DELETE_ON_CLOSE flag on Windows and TMPFILE on Linux
+    }
+
+    #[cfg(not(target_os = "linux"))]
+    {
+        // tempfile() adds FILE_FLAG_DELETE_ON_CLOSE flag on Windows and TMPFILE on Unix
         // so the file should be deleted when all file descriptors are closed
         tempfile::tempfile()
     }
