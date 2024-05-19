889a0642c561ddb3ef78b306554c1a1d22def066
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2024-05-19 12:06:22 +0200

    Don't panic in prepare_file_inputs

--- src/prepare_input.rs
+++ src/prepare_input.rs
@@ -4,6 +4,7 @@ use std::path::{Path, PathBuf};
 use rayon::iter::{IndexedParallelIterator, IntoParallelIterator};
 use rayon::vec::IntoIter;
 use crate::formatted_error::FormattedError;
+use crate::generic_utils::ResultExt;
 
 pub(crate) enum TestInputSource {
     File(PathBuf)
@@ -29,23 +30,33 @@ pub(crate) struct TestingInputs<T: IndexedParallelIterator<Item=Test>> {
 
 pub(crate) fn prepare_file_inputs(input_dir: &Path, in_ext: &str) -> Result<TestingInputs<IntoIter<Test>>, FormattedError> {
     let tests = read_dir(input_dir)
-        .expect("Cannot open input directory")
-        .map(|input| {
-            input.expect("Failed to read contents of input directory").path()
+        .map_err(|error| FormattedError::from_str(&format!("Cannot open input directory:\n{error}")))?
+        .map(|input| -> Result<PathBuf, FormattedError> {
+            let input = input
+                .map_err(|error| FormattedError::from_str(
+                    &format!("Failed to read contents of input directory:\n{error}")
+                ))?;
+            Ok(input.path())
         })
+        // This `filter` and `map` could be replaced by `filter_ok` and `map_ok`
+        // in the `itertools` crate
         .filter(|path| {
-            path.extension().is_some_and(|ext| {
-                ".".to_owned() + ext.to_str().unwrap_or("") == in_ext
+            path.is_err_or(|path| {
+                path.extension().is_some_and(|ext| {
+                    ".".to_owned() + ext.to_str().unwrap_or("") == in_ext
+                })
             })
         })
         .map(|file_path| {
-            let test_name = file_path.file_stem()
-                .and_then(OsStr::to_str)
-                .ok_or(FormattedError::from_str(&format!("The input file {} is invalid", file_path.display())))?
-                .to_owned();
-            Ok(Test {
-                test_name,
-                input_source: TestInputSource::File(file_path),
+            file_path.and_then(|file_path| {
+                let test_name = file_path.file_stem()
+                    .and_then(OsStr::to_str)
+                    .ok_or(FormattedError::from_str(&format!("The input file {} is invalid", file_path.display())))?
+                    .to_owned();
+                Ok(Test {
+                    test_name,
+                    input_source: TestInputSource::File(file_path),
+                })
             })
         })
         .collect::<Result<Vec<Test>, FormattedError>>()?;
